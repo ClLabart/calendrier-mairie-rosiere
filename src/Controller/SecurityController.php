8aa96303610408fd8d1672f0b967deffb7fd5d1c
CDiard <91243460+CDiard@users.noreply.github.com>
2022-12-29 23:05:54 +0100

    Back - Profil : Suppression de compte + Responsive

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use App\Entity\Date;
+use App\Entity\ResetPasswordRequest;
+use App\Entity\User;
+use App\Repository\BlogRepository;
+use App\Repository\DateRepository;
+use App\Repository\ResetPasswordRequestRepository;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,9 +22,9 @@ class SecurityController extends AbstractController
     #[Route(path: '/connexion', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('app_calendar');
-         }
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_calendar');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -28,5 +38,47 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/supprimer-compte', name: 'app_delete_user')]
+    public function deleteUser(UserRepository $userRepository, DateRepository $dateRepository, BlogRepository $blogRepository, ResetPasswordRequestRepository $resetRepository, ManagerRegistry $doctrine): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_calendar');
+        }
+        $userId = $this->getUser()->getId();
+
+        $this->container->get('security.token_storage')->setToken(null);
+
+        $entityManager = $doctrine->getManager();
+
+        $user = $userRepository->find($userId);
+
+        if ($user->getDates()) {
+            $date = $dateRepository->findOneBy([
+                'user' => $userId,
+            ]);
+
+            if ($date->getBlog()) {
+                $blog = $blogRepository->find($date->getBlog()->getId());
+                $entityManager->remove($blog);
+            }
+
+            $entityManager->remove($date);
+        }
+
+        $resetPassword = $resetRepository->findOneBy([
+            'user' => $this->getUser(),
+        ]);
+
+        if ($resetPassword) {
+            $entityManager->remove($resetPassword);
+        }
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('deleted','Votre compte a été supprimé.');
+        return $this->redirectToRoute('app_calendar', [], Response::HTTP_SEE_OTHER);
     }
 }
